3d00f2bf36d6c4018b81b1917264c536d4afd93d
SreeHarshan <sreeerode12@gmail.com>
2023-02-13 10:48:24 +0530

    added login backend

---
+++ README.md
@@ -0,0 +1,6 @@
+Chat app designed during an event taking place in my college called EHORYZON'23
+
+
+* Flutter frontend 
+
+* Flask backend
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,request
 
 import psycopg2 as psy
 
@@ -9,6 +9,43 @@ app = Flask(__name__)
 # db connection
 con = psy.connect("postgres://izjwxjys:Y0Pky9Xhpdq5Yd_Ajhyg3d6onKhFIJbf@tiny.db.elephantsql.com/izjwxjys")
 
+
+@app.route("/login",methods=["GET"])
+def login():
+    args = request.args.to_dict()
+   
+    # check if args are in api
+    if(args["uname"] and args["pass"]):
+        # check in db
+        cur = con.cursor()
+        cur.execute("Select * from credentials where id='{}' AND pass='{}'".format(args["uname"],args["pass"]))
+        val = cur.fetchone()
+
+        if(cur.rowcount!=0):
+            cur.close()
+            return {"login":True}
+        cur.close()
+        return {"login":False}
+    return {"Output":"args not present"}
+
+@app.route("/adduser",methods=["GET"])
+def adduser():
+    args = request.args.to_dict()
+    
+    # check if args are in api
+    if(args["uname"] and args["pass"]):
+        # Check if the user already exists
+        cur = con.cursor()
+        cur.execute("Select * from credentials where id='{}' AND pass='{}'".format(args["uname"],args["pass"]))
+        
+        if(cur.rowcount==0):
+            # add the values in db
+            cur.execute("INSERT INTO credentials(id,pass) VALUES('{}','{}')".format(args["uname"],args["pass"]))
+            con.commit()
+            cur.close()
+            return {"Output":"Value was added"}
+        return {"Output":"Value already exists"}
+    return {"Output":"args not present"}
 
 # Temp one
 @app.route("/")
